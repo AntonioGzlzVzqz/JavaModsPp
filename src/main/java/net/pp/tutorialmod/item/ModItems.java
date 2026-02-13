e8f38b7ffda0be1d880524841d4a9102456f822d
AntonioGzlzVzqz <178310997+AntonioGzlzVzqz@users.noreply.github.com>
2026-02-12 21:01:18 -0600

    Aprendimos a hacer ModItems en Minecraft

---
+++ src/main/java/net/pp/tutorialmod/item/ModItems.java
@@ -0,0 +1,32 @@
+package net.pp.tutorialmod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.pp.tutorialmod.TutorialMod;
+
+
+public class ModItems {
+
+    public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
+    public static final Item LA_PVTA_RED_COLA = registerItem("la_pvta_red_cola", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        TutorialMod.LOGGER.info("Registration ModItems for " + TutorialMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(PINK_GARNET);
+            fabricItemGroupEntries.add(RAW_PINK_GARNET);
+            fabricItemGroupEntries.add(LA_PVTA_RED_COLA);
+        });
+    }
+}
